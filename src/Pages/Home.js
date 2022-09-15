03a81f0a5246929e2fa94fff53263ec724944bdb
sragu2000 <ragurajsivanantham@gmail.com>
2022-09-15 21:23:49 +0530

    homw page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,57 @@
 import React from "react";
-function Home(){
-    return(
+function Home() {
+    return (
         <React.Fragment>
-            From Home Page
+            <div className="container mt-3">
+                <div className="card">
+                    <div className="card-header">Purchase</div>
+                    <div className="card-body">
+                        <div className="row">
+                            <div className="col-md-8">
+                                <input type="text" className="form-control" placeholder="Purchase Invoice Number"></input>
+                            </div>
+                            <div className="col-md-4">
+                                <select className="form-control">
+                                    <option disabled selected>Select Supplier</option>
+                                    <option>Option2</option>
+                                </select>
+                            </div>
+                        </div>
+                        <input type="date" placeholder="Purchase Date" className="mt-3 form-control"></input>
+                        <div className="row mt-3">
+                            <div className="col-md-6">
+                                <select className="form-control">
+                                    <option selected disabled>Select Product</option>
+                                </select>
+                            </div>
+                            <div className="col-md-3">
+                                <select className="form-control">
+                                    <option selected disabled>Select Type</option>
+                                </select>
+                            </div>
+                            <div className="col-md-3">
+                                <input type="number" className="form-control" placeholder="Quantity"></input>
+                            </div>
+                        </div>
+                        <div className="row mt-3">
+                            <div className="col-md-6">
+                                <input type="number" className="form-control" placeholder="Purchase Price"></input>
+                            </div>
+                            <div className="col-md-6">
+                                <input type="number" className="form-control" placeholder="Retail Price"></input>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-footer">
+                        <div className="row">
+                            <div className="col-md-6">
+                                <button type="submit"></button>
+                            </div>
+                            <div className="col-md-6"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </React.Fragment>
     );
 }
--- src/Routes/Ourroutes.js
+++ src/Routes/Ourroutes.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../Pages/Home";
+import Login from "../Pages/Login";
 const Ourroutes = () => {
   return (
     <React.Fragment>
       <BrowserRouter>
         <Routes>
           <Route exact path="/home" element={<Home />} />
-          <Route exact path="/" element={<Home />} />
+          <Route exact path="/" element={<Login />} />
         </Routes>
       </BrowserRouter>
 
